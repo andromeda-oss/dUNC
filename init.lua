7018f8a670448a3471b313b67a0361d73ab2aff0
Seasonal <seasoank@gmail.com>
2025-05-27 23:21:31 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,20 @@
-local function create_test(name: string, callback)
-    local directory = 
-    local file = 
+local function create_test(folder, name, callback, skip)
+    if skip then return end
+
+    local url = "https://raw.githubusercontent.com/andromeda-oss/dUNC/main/functions/"
+    local file = url .. folder .. "/" .. name .. ".lua"
+
+    local func, req = loadstring(game:HttpGet(file))()
+
+    if type(func) ~= "function" then
+        error("First return value from test file must be a function")
+        return
+    end
+
+    callback(func, req)
 end
+
+create_test("closures", "checkcaller", function(func, req)
+    print(req)
+    local result = func()
+end, false)
